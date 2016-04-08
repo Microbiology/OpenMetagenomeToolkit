8163bc5f628ae0c947dde7b13b21fffe563bbc74
Geoffrey Hannigan <ghanni@upenn.edu>
2016-04-08 07:56:42 -0400

    Updated some syntax for best practices.

--- bin/remove_block_fasta_format.pl
+++ bin/remove_block_fasta_format.pl
@@ -9,31 +9,34 @@
 use strict;
 use warnings;
 
+my $IN;
+my $OUT;
+
 # Set files to scalar variables
 my $usage = "Usage: perl $0 <INFILE> <OUTFILE>";
 my $infile = shift or die $usage;
 my $outfile = shift or die $usage;
-open(IN, "<$infile") || die "Unable to open $infile: $!";
-open(OUT, ">$outfile") || die "Unable to write to $outfile: $!";
+open($IN, "<", "$infile") || die "Unable to open $infile: $!";
+open($OUT, "<", "$outfile") || die "Unable to write to $outfile: $!";
 
 #Set variables to be used later in script
 my $flag = 0;
 
-while (my $line = <IN>) {
+while (my $line = <$IN>) {
 	#If the line is a fasta title line (starts with arrow)
 	if ($line =~ /\>/) {
-		print OUT "\n" if ($flag);
+		print $OUT "\n" if ($flag);
 		$flag=1;
 	}
 	else {
 		chomp $line;
 	}
-		print OUT $line;
+		print $OUT $line;
 }
 # Print the final newline
-print OUT "/n";
+print $OUT "/n";
 
 #Close out files and print completion note to STDOUT
-close(IN);
-close(OUT);
+close($IN);
+close($OUT);
 print "Completed\n"
